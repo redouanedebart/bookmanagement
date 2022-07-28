4d424172e269e9f5776fc63ae835730d43402b13
redouanedebart <77845511+redouanedebart@users.noreply.github.com>
2022-07-28 10:40:58 +0200

    begin

--- bookmanagement.cpp
+++ bookmanagement.cpp
@@ -1,1 +1,34 @@
-#include<stdlib.h>
+//---------Headers-------
+
+#include <iostream>
+#include <windows.h> //might not be necessary
+#include <mysql.h> //might change to use postgreSQL
+#include <sstream>
+#include <conio.h>
+
+#define HOST "127.0.0.1"
+#define USER "root"
+#define PASS /*put the password of MySQL in here between ""*/
+#define DATABASE "Management"
+#define PORT 3306
+#define PASSWORD //pwd for the app
+
+//--------------------------
+//--------Global Var---------
+typedef struct
+{
+	int date;
+	int month;
+	int year;
+}date;
+//--------------------------
+//---------Classes----------
+
+class book{
+    int id; //will be PK for the database
+    string title;
+    string author;
+    int page_number;
+    int qty;
+    float price;
+}
