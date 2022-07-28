00adcfb1653e84276a12807304030f41d3e76aaf
redouanedebart <77845511+redouanedebart@users.noreply.github.com>
2022-07-28 11:00:47 +0200

    add supplier class

--- bookmanagement.cpp
+++ bookmanagement.cpp
@@ -15,20 +15,45 @@
 
 //--------------------------
 //--------Global Var---------
+
 typedef struct
 {
 	int date;
 	int month;
 	int year;
 }date;
+
 //--------------------------
 //---------Classes----------
 
 class book{
-    int id; //will be PK for the database
-    string title;
-    string author;
-    int page_number;
-    int qty;
-    float price;
-}
+        int id; //will be PK for the database
+        string title;
+        string author;
+        int page_number;
+        int qty;
+        float price;
+
+    public:
+        void add();
+        void update_price();
+        void search();
+        void update_qty();
+        void display();
+
+};
+
+class supplier{
+        int id; //PK in the DB
+        string name;
+        long int phn; //phone ? would probably use a string instead
+        string addr_line1;
+        string addr_line2;
+        string addr_city;
+        string addr_state;
+    public:
+        void add_supplier();
+        void search_supplier_by_id();
+        void remove_supplier();
+
+};
